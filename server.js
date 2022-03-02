94cbed80e44700afe344df1b6a089b0aa1da3033
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-02 12:12:25 -0800

    Build routes for MVP

--- server.js
+++ server.js
@@ -3,20 +3,48 @@ const heuristics = require('./wordle_guess_heuristics.json');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Wordle Guess Heuristics';
+app.set('port', process.env.PORT || 3010);
+app.locals.title = 'Referle Database';
 app.locals.heuristics = heuristics;
 
 app.get('/', (request, response) => {
   response.send('Oh hey Wordle lovers');
 });
 
-app.get('/api/v1/heuristics', (request, response) => {
-  let heuristics = app.locals.heuristics;
-  // console.log(app.locals.heuristics)
-  response.json({ heuristics });
+app.get('/api/v1/heuristics/sorted/avg_tile_score', (request, response) => {
+  console.log('REQUEST BEGINS --------->', request.route.path, '<---------- REQUEST ENDS');
+  const heuristics = app.locals.heuristics;
+  let sortedHeuristics = [...heuristics];
+    sortedHeuristics = sortedHeuristics.sort((a, b) => parseFloat(b['avg_tile_score']) - parseFloat(a['avg_tile_score']))
+      .filter(word => {
+        let index = 0;
+        let result = true;
+        for( const letter of word.guess) {
+          if (word.guess.indexOf(letter) !== index) {
+            result = false;
+            break;
+          }
+          index++;
+        }
+        return result;
+      })
+  response.json(sortedHeuristics);
 });
+
+app.get('/api/v1/heuristics/bookmarked', (request, response) => {
+  const  heuristics = app.locals.heuristics;
+  const bookmarkedWords = heuristics.filter(word => word.isBookmarked);
+  response.json(bookmarkedWords);
+})
+
+app.patch('/api/v1/heuristics/:id', (request, response) => {
+  const heuristics = app.locals.heuristics;
+  const index = heuristics.findIndex(word => word.id = request.params.id)
+  heuristics[index].isBookmarked = request.body.isBookmarked;
+  response.json(heuristics[index]);
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
